d4d5359f3aa5f0602359eafe0495d0264f6bcf68
Md-Abdur-Raheem <md.raheem2450@gmail.com>
2021-11-09 18:03:38 +0600

    .env updated

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./edumark-1f8c9-firebase-adminsdk-3ru7m-148e0e617d.json");
+// const serviceAccount = require("./edumark-1f8c9-firebase-adminsdk-3ru7m-148e0e617d.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
